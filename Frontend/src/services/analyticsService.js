fa23e453725e4284a14e3c0e55f4e1610febf5a8
Abhishek Choudhary <abhishekchoudhary38808@gmail.com>
2026-03-01 17:28:16 +0530

    fix the analyticsService.js

--- Frontend/src/services/analyticsService.js
+++ Frontend/src/services/analyticsService.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const getConfidenceAnalytics = async() => {
     const token = localStorage.getItem("token");
-    return axios.get("http://localhost:8080/api/analytics/confidence", {
+    return axios.get("/api/analytics/confidence", {
         headers: {
             Authorization: `Bearer ${token}`,
         },
